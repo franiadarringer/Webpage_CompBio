ba08855d6efb5b3c41373499970696ec7c0bd48f
franiadarringer <frances.darringer@uvm.edu>
2026-03-17 14:15:39 -0400

    class 3/17

---
+++ python_scripts/python_intro.py
@@ -0,0 +1,39 @@
+# frania darringer 3/17
+
+# installing libraries
+import numpi as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+out = scaler * 3 # doing math with object
+
+myList = [34, 7, 98] # create a list
+
+myList.append(33) # so appending data in a list
+
+len(myList) # length function
+
+# make a list of colors
+a_list = ["blue", "green", "red"]
+
+# indexing into a list
+a_list[0]
+print(a_list[0])
+
+# looking at data types
+nums = [1, 2, 5, 8]
+chars = ["a", "b", "c"]
+boolean = [True, True, False]
+
+# mixed lists
+mixed = [1, 3, True, "blue", 5]
+
+# neg indexing
+mixed[-1] # returns last element
+mixed[-3] # third element from right
+
+# checking data types
+type(nums[0]) # type returns highest level object type
+
+# ranged indexing
+mixed[1:4]
